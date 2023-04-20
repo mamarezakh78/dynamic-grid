d846147e24b0ad5f2e827f943999e2f462835699
mamareza khatami <smrkh78@gmail.com>
2023-04-20 14:19:20 +0330

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/grid/grid.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'grid',
+    templateUrl: './grid.component.html',
+    styleUrls: ['./grid.component.css'],
+    imports: [
+        CommonModule
+    ]
+})
+export class GridComponent {
+
+    
+
+}
