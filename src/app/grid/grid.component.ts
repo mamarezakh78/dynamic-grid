b442c0dc715a3d8052d1e8484bc4a250bbf1b8a7
mamareza khatami <smrkh78@gmail.com>
2023-04-20 20:38:41 +0330

    [feat] create grid component

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,16 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 @Component({
     standalone: true,
     selector: 'grid',
     templateUrl: './grid.component.html',
     styleUrls: ['./grid.component.css'],
     imports: [
-        CommonModule
+        CommonModule,
+        MatMenuModule,
+        MatButtonModule,
+        MatCheckboxModule
     ]
 })
 export class GridComponent {
+
+    @Input() title: string = "Title";
 
     
 
